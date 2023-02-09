6ff7b1102fac11967da48ba3463e665c3e473855
gitsammy09 <sammyfooty09@gmail.com>
2023-02-09 17:33:27 +1100

    Create-Post-Request

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+const router = express.Router();
+
+const users = [];
+
+router.get('/', (req, res) => {
+    console.log(users);
+    res.send(users);
+});
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    users.push({ ...user, id: uuidv4() });
+    
+    res.send(`User with the name ${user.firstName} added to the database`);
+});
+
+export default router;
