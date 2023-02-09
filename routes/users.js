7981cbada4a5ae79ede8495e371f101f54b6bb28
gitsammy09 <sammyfooty09@gmail.com>
2023-02-09 18:10:23 +1100

    Create-Delete-Request

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.get('/', (req, res) => {
     console.log(users);
@@ -18,4 +18,19 @@ router.post('/', (req, res) => {
     res.send(`User with the name ${user.firstName} added to the database`);
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id);
+    
+    res.send(foundUser);
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+
+    res.send(`User with the id ${id} is deleted form the database`)
+});
 export default router;
